dc50fae316645e0a57ac6b179f3c058276be0a4f
danielcordovam-eng <da.cordovam@duocuc.cl>
2026-06-14 20:03:11 -0400

    Funcion para mostrar opciones (Menu)

---
+++ Ejercicio01/E001.py
@@ -0,0 +1,11 @@
+def Opciones_menu():
+    print('''\n
+========== MENÚ PRINCIPAL ==========
+1. Agregar producto
+2. Buscar producto
+3. Eliminar producto
+4. Actualizar disponibilidad
+5. Mostrar productos
+6. Salir
+=====================================
+\n''')
